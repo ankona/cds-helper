d9de7cd28864571951bfec20385170bc9e8c887d
Christopher McBride <3595025+ankona@users.noreply.github.com>
2025-06-11 18:14:56 -0400

    add dataset request handler for CDS

--- cds_helper/cli/request.py
+++ cds_helper/cli/request.py
@@ -8,6 +8,7 @@ from cds_helper.core.core import (
     download_monthly_data,
 )
 
+DATE_FORMAT = "%Y-%m-%d"  # "%Y-%m-%d %H:%M:%S"
 
 
 def help_text() -> str:
@@ -23,11 +24,47 @@ def command() -> str:
     return "request"
 
 
+def _format_date(date_str: str) -> datetime:
+    """Convert a date string to a datetime object using the default format.
+
+    Parameters
+    ----------
+    date_str : str
+        The date string to convert.
+
+    Returns
+    -------
+    datetime
+        The converted datetime.
+    """
+    return datetime.strptime(  # noqa: DTZ007
+        date_str,
+        DATE_FORMAT,
+    )
+
+
 def configure_parser(parser: argparse.ArgumentParser) -> None:
     """Configure the parser for this sub-command."""
-    parser.add_argument("file", help="Path to a file containing request details.")
+    parser.add_argument("path", help="Path to a file containing request details.")
+    parser.add_argument("output", help="Path to write the retrieved datasets to.")
 
 
 def handle(args: argparse.Namespace) -> None:
     """Process a CLI request."""
-    print("empty request handler")
+    path = pathlib.Path(args.path).resolve()
+    if not path.exists():
+        raise FileNotFoundError(f"A file does not exist at {path}.")
+
+    output_to = pathlib.Path(args.output).resolve()
+    if not output_to.exists():
+        output_to.mkdir(parents=True)
+
+    with path.open() as fp:
+        request_dict = json.load(fp)
+
+    request_dict["start_date"] = _format_date(request_dict["start_date"])
+    request_dict["end_date"] = _format_date(request_dict["end_date"])
+
+    file_map = request_dict.pop("file_map")
+    request = CdsDataRequest(**request_dict)
+    download_monthly_data(request, file_map, output_to)
